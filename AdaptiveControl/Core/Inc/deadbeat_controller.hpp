59a3b1b34ff2eed4ff8e29ca73f57ec1cbdfedd6
User <User@DESKTOP-23QVBVH>
2020-09-01 23:10:30 +0200

    - start with implementation of deadbeat controller class - only two methods left before first test

--- AdaptiveControl/Core/Inc/deadbeat_controller.hpp
+++ AdaptiveControl/Core/Inc/deadbeat_controller.hpp
@@ -10,12 +10,32 @@
 
 // this class implements an adaptive deadbeat controller source "Adaptive und Lernende Regelungen v. Karl Kleinmann"
 // use deadbeat controller only for asymptotic stable systems !
+// the deadbeat controller tries settle the system in n+1 steps (with n as order of the system to be controlled)
+// we can choose the first control output
 
 
 class deadbeat_controller
 {
+private:
+	int maxControlOutput;
+	int firstControlOutput;
+	int deadtime;
+	int order;
+	// this is the coefficient p array of the controller
+	float* pArray;
+	// this is the coefficient q array of the controller
+	float* qArray;
+	// this is the coefficient a array of the system
+	float* aArray;
+	// this ist the coefficient b array of the system
+	float* bArray;
 
-
+public:
+	deadbeat_controller(int maxcontroloutput, int firstcontroloutput, int order);
+	~deadbeat_controller();
+	void getNewSystem(float* parray, float* qarray);
+	void calculateNewController();
+	float controll(float input);
 };
 
 
--- AdaptiveControl/Core/Src/deadbeat_controller.cpp
+++ AdaptiveControl/Core/Src/deadbeat_controller.cpp
@@ -6,5 +6,47 @@
  */
 
 
+#include "deadbeat_controller.hpp"
+#include "debug.h"
+#include <stdio.h>
+
+
+deadbeat_controller::deadbeat_controller(int maxcontroloutput, int firstcontroloutput, int order)
+:maxControlOutput(maxcontroloutput), firstControlOutput(firstcontroloutput), order(order),pArray(new float[order]), qArray(new float[order]),
+ aArray(new float*[order]), bArray(new float*[order])
+{
+}
+
+deadbeat_controller::~deadbeat_controller()
+{
+	delete pArray;
+	delete qArray;
+}
+
+void getNewSystem(float* system)
+{
+
+	for(int i=(order-1); i>0; i--)
+		{
+		aArray[i] = system[i];
+		}
+
+		for(int i=m; i>order; i--)
+		{
+		bArray[i] = system[i];
+		}
+}
+
+void calculateNewController()
+{
+	// to be defined
+}
+
+float controll(float input)
+{
+	// to be defined
+}
+}
+
 
 
